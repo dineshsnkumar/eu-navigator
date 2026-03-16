491a3b66e01ae8ec14ab730042069fc6d9eb1296
Dinesh <dinesh@Dineshs-MacBook-Air.local>
2026-03-16 14:50:19 +0530

    Inital setup for building RAG pipeline

---
+++ eu-navigator-api/scripts/setup_rag.py
@@ -0,0 +1,18 @@
+def load_documents(filepath):
+    """Load documents from the jsonl"""
+    pass
+
+def create_embeddings():
+    """Create embedding model"""
+    pass
+
+def create_vector_db():
+    """Create vector db"""
+    pass
+
+
+def main():
+    print("Setting up RAG pipeline")
+
+if __name__== "__main__":
+    main()
